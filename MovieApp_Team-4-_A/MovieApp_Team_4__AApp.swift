aa81a4037dca02e5a348268341155421d14f78de
Rana <rana.k.alngashy@gmail.com>
2025-12-28 15:46:11 +0300

    Initial Commit

---
+++ MovieApp_Team-4-_A/MovieApp_Team_4__AApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp_Team_4__AApp.swift
+//  MovieApp_Team-4-_A
+//
+//  Created by Rana Alngashy on 08/07/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp_Team_4__AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
